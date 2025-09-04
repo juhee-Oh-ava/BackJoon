d19f7879be71c720d2fe544fa772eddf46f4121b
juhee-Oh-ava <zchrx7625@naver.com>
2025-09-04 22:53:27 +0900

    [Bronze III] Title: 통학의 신, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/17945. 통학의 신/통학의 신.py
+++ 백준/Bronze/17945. 통학의 신/통학의 신.py
@@ -1,21 +1,7 @@
 A, B = map(int, input().split())
-check = False
-result = []
-x1 = int(-A + ((A**2 - B)**0.5))
-x2 = int(-A - ((A**2 - B)**0.5))
-lst = [x1, x2]
+li = []
+for i in range(-1000, 10001):
+    if i*(2*A-i) == B:
+        li = list(set([-i, -(2*A-i)]))
 
-if x1 % 1 == 0 and x2 % 1 == 0:
-    result = sorted(lst)
-    check = True
-    # print(*result)
-    if x1 == x2:
-    # print(x1)
-        check = False
-
-if check:
-    print(*result)
-else:
-    print(x1)
-
-
+print(*sorted(li))
